e3754e30b34a7966205c85da7dbcc7f17b195977
firebolt55439 <sumer112@gmail.com>
2017-01-28 18:30:56 -0800

    Added Google Auth front-end (back-end WIP)

--- src/Login.js
+++ src/Login.js
@@ -10,13 +10,16 @@ export default class Login extends Component {
 		return (
 			<form className='loginForm' onSubmit={this.handleSubmit}>
 				<h3>Login</h3>
+				<label htmlFor="uid">Username:</label>
 				<div className="input-group">
 				  <input type="text" className="form-control" placeholder="Username" ref='uid' />
 				  <span className="input-group-addon" id="basic-addon1">@students.harker.org</span>
 				</div>
-				<div className="input-group">
+				<div className="form-group">
+				  <label htmlFor="pwd">Password:</label>
 				  <input type="password" className="form-control" placeholder="Password" ref='pwd' />
 				</div>
+				<div id="firebaseui-auth-container"></div>
         	</form>
 			);
 	}
