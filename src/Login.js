475a7f97b4cfbeaa52effbba76859ca86232733d
firebolt55439 <sumer112@gmail.com>
2017-01-28 18:52:44 -0800

    Got Google Login working

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,71 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 
 export default class Login extends Component {
+
+	authenticateUser(x) {
+		// Init auth
+		var config = {
+			apiKey: "AIzaSyDGzHdJ-4B35kuShuJCgmHhkbBy_nMCvy4",
+			authDomain: "harker-courses.firebaseapp.com",
+			databaseURL: "https://harker-courses.firebaseio.com",
+			storageBucket: "harker-courses.appspot.com",
+			messagingSenderId: "125151666633"
+		};
+		firebase.initializeApp(config);
+
+		// Install state change handler
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				var displayName = user.displayName;
+	            var email = user.email;
+	            var emailVerified = user.emailVerified;
+	            var photoURL = user.photoURL;
+	            var uid = user.uid;
+	            var providerData = user.providerData;
+	            user.getToken().then(function(accessToken) {
+					console.log(JSON.stringify({
+  	                displayName: displayName,
+  	                email: email,
+  	                emailVerified: emailVerified,
+  	                photoURL: photoURL,
+  	                uid: uid,
+  	                accessToken: accessToken,
+  	                providerData: providerData
+				}, null, '  '));
+					/*
+	              document.getElementById('sign-in-status').textContent = 'Signed in';
+	              document.getElementById('sign-in').textContent = 'Sign out';
+	              document.getElementById('account-details').textContent = JSON.stringify({
+	                displayName: displayName,
+	                email: email,
+	                emailVerified: emailVerified,
+	                photoURL: photoURL,
+	                uid: uid,
+	                accessToken: accessToken,
+	                providerData: providerData
+	              }, null, '  ');
+					*/
+	            });
+			} else {
+				/*
+				// User is signed out.
+				document.getElementById('sign-in-status').textContent = 'Signed out';
+				document.getElementById('sign-in').textContent = 'Sign in';
+				document.getElementById('account-details').textContent = 'null';
+				*/
+			}
+		}, function(error) {
+			console.log(error);
+		});
+	}
 
 	handleSubmit(event) {
 
 	}
 
 	render() {
+		this.authenticateUser();
 		return (
 			<form className='loginForm' onSubmit={this.handleSubmit}>
 				<h3>Login</h3>
