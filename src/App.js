9616b10caeab67db5760d986fcc91a535af37c0e
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-02-02 09:24:14 -0800

    add reviewform routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Router, Route, hashHistory } from 'react-router'
 
-import ReviewForm from "./ReviewForm"; //save for later
+import ReviewForm from "./ReviewForm";
 import Login from "./Login";
 
 const Home = () => <div><h1>welcome to harker courses</h1><a href="/#/login">login</a></div>
@@ -13,6 +13,7 @@ export default class App extends Component {
       <Router history={hashHistory}>
         <Route path='/' component={Home} />
         <Route path='/login' component={Login} />
+        <Route path='/review' component={ReviewForm} />
       </Router>
     );
   }
