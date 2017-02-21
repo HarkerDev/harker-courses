a43ddcf315b4ffdbf0d58a917a0c4eda75c1c697
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-02-21 10:30:46 -0800

    render course names on browse courses page

--- src/CourseBrowser.js
+++ src/CourseBrowser.js
@@ -2,44 +2,48 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import coursesData from '../courses.json';
 
-var coursesArr = [];
-for(var key in coursesData) {
-    if (coursesData[key]!=null) {
-        coursesArr.push([key, coursesData[key]]);
-    }
+for (var key in coursesData) {
+  if (coursesData[key]!=null) {
+    console.log("coursesData[key].title is " + coursesData[key].title);
+  }
 }
 
 //form for adding a review
 export default class CourseBrowser extends Component {
 
-    constructor(props){
-        super(props);
+  constructor(props){
+    super(props);
 
-		// TODO: Download all course reviews from database
-		var postsRef = firebase.database().ref('harker-courses').child("posts");
-		postsRef.on('value', function(snapshot) {
-			snapshot.forEach(function(childSnapshot) {
-				var childData = childSnapshot.val();
-				console.log(childData);
-			});
-		})
-    }
+    // TODO: Download all course reviews from database
+    var postsRef = firebase.database().ref('harker-courses').child("posts");
+    postsRef.on('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var childData = childSnapshot.val();
+        console.log(childData);
+      });
+    })
 
-    render() {
-        // TODO: Add search option (maybe to navbar top-right?)
-		// For now, lists courses with number of reviews
-		// TODO: Add star in ratings next to course name
-        return (
-			<div>
-				<h3 className="text-center">Browse All Courses</h3>
-	            <div className="list-group">
-					<a className="list-group-item">
-						<span className="badge">14</span>
-						<h4 className="list-group-item-heading">Course Name</h4>
-						<p className="list-group-item-text">Course description here...</p>
-					</a>
-				</div>
-			</div>
-        );
+    //render course names
+    for (var key in coursesData) {
+      var course = document.createElement("LI");
+      var title = document.createTextNode(coursesData[key].title);
+      course.appendChild(title);
+      document.body.appendChild(course);
     }
+  };
+
+  // TODO: Add search option (maybe to navbar top-right?)
+  // For now, lists courses with number of reviews
+  // TODO: Add star in ratings next to course name
+  // <div className="list-group">
+  // <a className="list-group-item">
+  // <h4 className="list-group-item-heading">Course Name</h4>
+  // <p className="list-group-item-text">Course description here...</p></a></div>
+  render() {
+    return (
+      <div>
+      <h3 className="text-center">Browse All Courses</h3>
+      </div>
+    );
+  }
 }
