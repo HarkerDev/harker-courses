647c1c58d2045237eba2568c72c18daa1f35e9bc
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-06-01 14:38:40 -0700

    add search functionality

--- src/CourseBrowser.js
+++ src/CourseBrowser.js
@@ -1,9 +1,139 @@
 import React, { Component } from 'react';
+import Autosuggest from 'react-autosuggest';
 import * as firebase from 'firebase';
 import coursesData from '../courses.json';
 
 var courseIDS = [];
 var currentIDS = [];
+
+const languages = [
+    {
+        title: '1970s',
+        languages: [
+            {
+                name: 'C',
+                year: 1972
+            }
+        ]
+    },
+    {
+        title: '1980s',
+        languages: [
+            {
+                name: 'C++',
+                year: 1983
+            },
+            {
+                name: 'Perl',
+                year: 1987
+            }
+        ]
+    },
+    {
+        title: '1990s',
+        languages: [
+            {
+                name: 'Haskell',
+                year: 1990
+            },
+            {
+                name: 'Python',
+                year: 1991
+            },
+            {
+                name: 'Java',
+                year: 1995
+            },
+            {
+                name: 'Javascript',
+                year: 1995
+            },
+            {
+                name: 'PHP',
+                year: 1995
+            },
+            {
+                name: 'Ruby',
+                year: 1995
+            }
+        ]
+    },
+    {
+        title: '2000s',
+        languages: [
+            {
+                name: 'C#',
+                year: 2000
+            },
+            {
+                name: 'Scala',
+                year: 2003
+            },
+            {
+                name: 'Clojure',
+                year: 2007
+            },
+            {
+                name: 'Go',
+                year: 2009
+            }
+        ]
+    },
+    {
+        title: '2010s',
+        languages: [
+            {
+                name: 'Elm',
+                year: 2012
+            }
+        ]
+    }
+];
+
+function escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+    const escapedValue = escapeRegexCharacters(value.trim());
+
+    if (escapedValue === '') {
+        return [];
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i');
+
+    return languages
+        .map(section => {
+            return {
+                title: section.title,
+                languages: section.languages.filter(language => regex.test(language.name))
+            };
+        })
+        .filter(section => section.languages.length > 0);
+}
+
+function getSuggestionValue(suggestion) {
+    return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+    return (
+        <span>{suggestion.name}</span>
+    );
+}
+
+function renderSectionTitle(section) {
+    return (
+        <strong>{section.title}</strong>
+    );
+}
+
+function getSectionSuggestions(section) {
+    return section.languages;
+}
+
+
 
 //keep all course ids
 for (var key in coursesData) {
@@ -18,6 +148,11 @@ export default class CourseBrowser extends Component {
         super(props);
 
         this.categoryRender = this.categoryRender.bind(this);
+
+        this.state = {
+            value: '',
+            suggestions: []
+        };
 
         // TODO: Download all course reviews from database
         var postsRef = firebase.database().ref('harker-courses').child("posts");
@@ -87,9 +222,43 @@ export default class CourseBrowser extends Component {
 
     }
 
+    onChange = (event, { newValue, method }) => {
+        this.setState({
+            value: newValue
+        });
+    };
+
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+            suggestions: getSuggestions(value)
+        });
+    };
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
     render() {
+        const { value, suggestions } = this.state;
+        const inputProps = {
+            placeholder: "Type 'c'",
+            value,
+            onChange: this.onChange
+        };
         return (
             <div>
+                <Autosuggest
+                    multiSection={true}
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    renderSectionTitle={renderSectionTitle}
+                    getSectionSuggestions={getSectionSuggestions}
+                    inputProps={inputProps} />
                 <h3 className="text-center">Browse All Courses</h3>
                 <div id="categories">
                     <ul>
