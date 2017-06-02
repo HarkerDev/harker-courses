80fd5086b8ca7914ac8590f1df732cf66cedf42f
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-06-01 20:49:00 -0700

    fix suggestion variable, fix search styling

--- src/CourseBrowser.js
+++ src/CourseBrowser.js
@@ -8,90 +8,7 @@ var coursesData = coursesDataCO;
 var courseIDS = [];
 var currentIDS = [];
 var courseCategories = [];
-
-var languages = [
-    {
-        title: '1970s',
-        languages: [
-            {
-                name: 'C',
-                year: 1972
-            }
-        ]
-    },
-    {
-        title: '1980s',
-        languages: [
-            {
-                name: 'C++',
-                year: 1983
-            },
-            {
-                name: 'Perl',
-                year: 1987
-            }
-        ]
-    },
-    {
-        title: '1990s',
-        languages: [
-            {
-                name: 'Haskell',
-                year: 1990
-            },
-            {
-                name: 'Python',
-                year: 1991
-            },
-            {
-                name: 'Java',
-                year: 1995
-            },
-            {
-                name: 'Javascript',
-                year: 1995
-            },
-            {
-                name: 'PHP',
-                year: 1995
-            },
-            {
-                name: 'Ruby',
-                year: 1995
-            }
-        ]
-    },
-    {
-        title: '2000s',
-        languages: [
-            {
-                name: 'C#',
-                year: 2000
-            },
-            {
-                name: 'Scala',
-                year: 2003
-            },
-            {
-                name: 'Clojure',
-                year: 2007
-            },
-            {
-                name: 'Go',
-                year: 2009
-            }
-        ]
-    },
-    {
-        title: '2010s',
-        languages: [
-            {
-                name: 'Elm',
-                year: 2012
-            }
-        ]
-    }
-];
+var subjects = [];
 
 function escapeRegexCharacters(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -106,14 +23,14 @@ function getSuggestions(value) {
 
     const regex = new RegExp('^' + escapedValue, 'i');
 
-    return languages
+    return subjects
         .map(section => {
             return {
                 title: section.title,
-                languages: section.languages.filter(language => regex.test(language.name))
+                subjects: section.subjects.filter(language => regex.test(language.name))
             };
         })
-        .filter(section => section.languages.length > 0);
+        .filter(section => section.subjects.length > 0);
 }
 
 function getSuggestionValue(suggestion) {
@@ -133,7 +50,7 @@ function renderSectionTitle(section) {
 }
 
 function getSectionSuggestions(section) {
-    return section.languages;
+    return section.subjects;
 }
 
 // Keep all course ids
@@ -142,17 +59,19 @@ function uniq(a) {
         return !pos || item != ary[pos - 1];
     })
 }
+
 var prettyCategories = {
     "Science - Life": "Life Sciences",
     "Science - Physical": "Physical Sciences"
 }
+
 for(var key in coursesData){
     var on = coursesData[key];
     if(on.subject in prettyCategories){
         on.subject = prettyCategories[on.subject];
     }
 }
-languages = [];
+
 for(var key in coursesData){
     courseIDS.push(key);
     var on = coursesData[key];
@@ -162,9 +81,9 @@ for(var key in coursesData){
 }
 courseCategories = uniq(courseCategories);
 for(var category of courseCategories){
-    languages.push({
+    subjects.push({
         title: category,
-        languages: Object.values(coursesData)
+        subjects: Object.values(coursesData)
             .filter(function(val){
                 return val.subject === category;
             })
@@ -254,22 +173,22 @@ export default class CourseBrowser extends Component {
     render() {
         const { value, suggestions } = this.state;
         const inputProps = {
-            placeholder: "Type 'c'",
+            placeholder: "Search for a course",
             value,
             onChange: this.onChange
         };
         return (
             <div>
                 <Autosuggest
-                    multiSection={true}
-                    suggestions={suggestions}
-                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                    getSuggestionValue={getSuggestionValue}
-                    renderSuggestion={renderSuggestion}
-                    renderSectionTitle={renderSectionTitle}
-                    getSectionSuggestions={getSectionSuggestions}
-                    inputProps={inputProps} />
+                    multiSection={ true }
+                    suggestions={ suggestions }
+                    onSuggestionsFetchRequested={ this.onSuggestionsFetchRequested }
+                    onSuggestionsClearRequested={ this.onSuggestionsClearRequested }
+                    getSuggestionValue={ getSuggestionValue }
+                    renderSuggestion={ renderSuggestion }
+                    renderSectionTitle={ renderSectionTitle }
+                    getSectionSuggestions={ getSectionSuggestions }
+                    inputProps={ inputProps } />
                 <h3 className="text-center">Browse All Courses</h3>
                 <div id="categories">
                     <ul>
