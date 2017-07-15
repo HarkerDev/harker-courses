ad96094f13f2b29284a2e0645cfd7d266c86c344
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-07-14 18:12:19 -0700

    update for lint errors, add nav spacing

--- src/CourseInfo.js
+++ src/CourseInfo.js
@@ -20,71 +20,77 @@ export default class CourseInfo extends Component {
     this.reviews = [];
     this.setUpListeners();
   }
-  
-  setUpListeners() {
-  	var postRef = firebase.database().ref().child('reviews').child(this.course);
-  	var that = this;
-  	postRef.on('child_added', function(data) {
-  		//addCommentElement(postElement, data.key, data.val().text, data.val().author);
-  		var data_key = data.key;
-  		data = data.val();
-  		data.key = data_key;
-  		that.reviews.push(data);
-  		that.forceUpdate();
-  	});
-  	postRef.on('child_changed', function(data) {
-  		var data_key = data.key;
-  		data = data.val();
-  		for(var i = 0; i < that.reviews.length; i++){
-  			var on = that.reviews[i];
-  			if(on.key === data_key){
-  				for(var key in data){
-  					that.reviews[i][key] = data[key];
-  				}
-  				break;
-  			}
-  		}
-  		that.forceUpdate();
-  	});
-  	postRef.on('child_removed', function(data) {
-  		var data_key = data.key;
-  		var idx = -1;
-  		for(var i = 0; i < that.reviews.length; i++){
-  			if(that.reviews[i].key === data_key){
-  				idx = i;
-  				break;
-  			}
-  		}
-  		that.reviews.splice(idx, 1); // delete review
-  		that.forceUpdate();
-  	});
-  	// (see https://firebase.google.com/docs/database/web/lists-of-data)
-  }
 
   componentWillMount() {
-  	/*
-    const script = document.createElement('script');
+    /*
+     const script = document.createElement('script');
 
-    script.innerHTML = "setTimeout(function() { \
-            $('.rating').rating(); \
-        }, 200);";
+     script.innerHTML = "setTimeout(function() { \
+     $('.rating').rating(); \
+     }, 200);";
 
-    document.body.appendChild(script);
-    */
+     document.body.appendChild(script);
+     */
+  }
+
+  setUpListeners() {
+    const postRef = firebase.database().ref().child('reviews').child(this.course);
+    const that = this;
+    postRef.on('child_added', (data) => {
+      // addCommentElement(postElement, data.key, data.val().text, data.val().author);
+      const dataKey = data.key;
+      data = data.val();
+      data.key = dataKey;
+      that.reviews.push(data);
+      that.forceUpdate();
+    });
+    postRef.on('child_changed', (data) => {
+      const dataKey = data.key;
+      data = data.val();
+      for (let i = 0; i < that.reviews.length; i++) {
+        const on = that.reviews[i];
+        if (on.key === dataKey) {
+          for (const key in data) {
+            that.reviews[i][key] = data[key];
+          }
+          break;
+        }
+      }
+      that.forceUpdate();
+    });
+    postRef.on('child_removed', (data) => {
+      const dataKey = data.key;
+      let idx = -1;
+      for (let i = 0; i < that.reviews.length; i++) {
+        if (that.reviews[i].key === dataKey) {
+          idx = i;
+          break;
+        }
+      }
+      that.reviews.splice(idx, 1); // delete review
+      that.forceUpdate();
+    });
+    // (see https://firebase.google.com/docs/database/web/lists-of-data)
   }
 
   render() {
-	return (
-	<div>
-	  {this.reviews.map(data =>
-		<div key={data.key}>
-			<h3>{data.authorName} {data.authorPhoto ? (<img src={data.authorPhoto} alt={data.authorName} />) : ""}</h3>
-				rated the course: {data.rating} star(s) <br />
-				and posted:<br />
-				<textarea className="form-control" readOnly value={data.review} />
-			</div>
-	  )}
-	</div>
-	);
+    return (
+      <div>
+        {this.reviews.map(data =>
+          (<div key={data.key}>
+            <h3>{data.authorName} {data.authorPhoto ?
+              (<img src={data.authorPhoto} alt={data.authorName} />) :
+              ''}</h3>
+            rated the course: {data.rating} star(s) <br />
+            and posted:<br />
+            <textarea
+              className="form-control"
+              readOnly
+              value={data.review}
+            />
+          </div>),
+        )}
+      </div>
+    );
   }
 }
