8a68254724057e22152778ac06f873fa383e9ea4
firebolt55439 <sumer112@gmail.com>
2017-07-14 17:52:44 -0700

    Minor textarea update

--- src/CourseInfo.js
+++ src/CourseInfo.js
@@ -81,7 +81,7 @@ export default class CourseInfo extends Component {
 			<h3>{data.authorName} {data.authorPhoto ? (<img src={data.authorPhoto} alt={data.authorName} />) : ""}</h3>
 				rated the course: {data.rating} star(s) <br />
 				and posted:<br />
-				<textarea readOnly value={data.review} />
+				<textarea className="form-control" readOnly value={data.review} />
 			</div>
 	  )}
 	</div>
